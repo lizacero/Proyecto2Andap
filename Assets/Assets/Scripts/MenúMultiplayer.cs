dc6411bc29523ad7c871934e691485f81e5a8920
Sebastian <gonzalezromerosebastian.901@gmail.com>
2025-07-07 08:51:35 -0500

    Se agrega PUN 2

---
+++ Assets/Assets/Scripts/MenúMultiplayer.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using TMPro;
+
+public class MenúMultiplayer : MonoBehaviour
+{
+    TMP_InputField buttonText;
+
+    public TMP_Text text;
+
+    public void OnclickConnect()
+    {
+
+    }
+}
