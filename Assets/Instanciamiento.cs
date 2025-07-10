490aea39c1e809ef594224eb3bacda9815208d04
Sebastian2092 <gonzalezromerosebastian.901@gmail.com>
2025-07-10 09:35:09 -0500

    Script Instanciamiento de Objetos

---
+++ Assets/Instanciamiento.cs
@@ -0,0 +1,12 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Instanciamiento : MonoBehaviour
+{
+    private void Start()
+    {
+        MenúMultiplayer.instance.InstanciarObjetos();
+    }
+
+
+}
